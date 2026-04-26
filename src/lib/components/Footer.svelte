4dd3ca8f66a7902e597629df67776f4f27d9dd6b
yay! <268245564+sillylume@users.noreply.github.com>
2026-04-26 20:37:14 +0200

    fix(ui): fix Footer.svelte padding for sm breakpoint

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,12 +2,12 @@
 	import Logo from './Logo.svelte';
 </script>
 
-<footer class="flex justify-between px-18 py-8 *:w-full">
+<footer class="flex justify-between px-6 py-8 *:w-full sm:px-18">
 	<div>
 		<Logo />
 		<p class="opacity-25">© 2026, All rights reserved.</p>
 	</div>
-<!-- 
+	<!-- 
     <div>
 		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit quos dolorem similique voluptates, sint officia obcaecati?</p>
 	</div> 
