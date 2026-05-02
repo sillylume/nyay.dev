e0791a645bf0c671e6d92784afad24b1411146fe
yay! <268245564+sillylume@users.noreply.github.com>
2026-05-02 22:34:40 +0200

    fix(ui): change Widget.svelte fade out-effect to slide

--- src/lib/components/widgets/Widget.svelte
+++ src/lib/components/widgets/Widget.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { fade, slide } from 'svelte/transition';
+	import { slide } from 'svelte/transition';
 
 	let props = $props();
 </script>
 
 <div
 	in:slide
-	out:fade
+	out:slide
 	{...props}
 	class={['bg-neutral-800/25 p-2 ring-1 ring-neutral-600/50', props.class]}
 >
